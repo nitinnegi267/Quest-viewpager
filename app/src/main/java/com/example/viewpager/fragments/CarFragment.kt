d8d017bd09273cb3ffdd17ef52e8a049717587ee
nitinnegixplor <144686880+nitinnegixplor@users.noreply.github.com>
2025-01-14 14:20:29 +0530

    My first commit of Viewpager

---
+++ app/src/main/java/com/example/viewpager/fragments/CarFragment.kt
@@ -0,0 +1,28 @@
+package com.example.viewpager.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.example.viewpager.R
+
+class CarFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val rootView = inflater.inflate(R.layout.fragment_car, container, false)
+        return rootView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        view.findViewById<TextView>(R.id.textview_car)
+
+    }
+}
